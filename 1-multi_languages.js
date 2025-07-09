f52ebdc8ae7c07b1927e6b8eb2846cf1fdddced8
Michael Boateng Mensah <mkboatmens17@gmail.com>
2025-07-09 17:05:09 +0000

    Add second task

---
+++ 1-multi_languages.js
@@ -0,0 +1,11 @@
+let language = ['C', 'Python', 'JavaScript'];
+
+console.log(`${language[0]} is fun`);
+console.log(`${language[1]} is cool`);
+console.log(`${language[2]} is amazing`);
+
+/*
+for (let i = 0; i < language.length; i++) {
+	console.log(`${language[0]} is fun`)
+
+*/
